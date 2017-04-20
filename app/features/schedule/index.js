09391ae6eacf360aa85c49edc6ca859588bda0f0
Yannik Sander <accounts@ysndr.de>
2017-04-20 19:03:15 +0200

    delete feature approach

--- app/features/schedule/index.js
+++
@@ -1,6 +0,0 @@
-// const model = require('./model')
-const constructors = require('./constructors')
-const methods = require('./methods')
-const api = require('./api')
-
-module.exports = { constructors, methods, api }
--- app/features/school/index.js
+++
@@ -1,5 +0,0 @@
-// const model = require('./model.js')
-  const api = require('./api')
-
-
-module.exports = { api }
